c4d73ca111668faf050b4a6f65fc9a3b7a244acd
Paul Rose <prose2334@gmail.com>
2018-05-13 14:59:21 -0400

    i give up

--- letter.js
+++ letter.js
@@ -1,10 +1,15 @@
-// Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+// Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder 
+// (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
 
 
 // A string value to store the underlying character for the letter
 // A boolean value that stores whether that letter has been guessed yet
-// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
+// A function that returns the underlying character if the letter has been guessed, or a placeholder 
+// (like an underscore) if the letter has not been guessed
+
+// A function that takes a character as an argument and checks it against the underlying character, 
+//updating the stored boolean value to true if it was guessed correctly
 
 
 function Letter (letterToGuess) {
@@ -16,11 +21,20 @@ function Letter (letterToGuess) {
 
     this.letterGuessResult = function () {
         // make the underlying letter appear if it has been guessed or an underscore if the letter has not been guessed
+        if(this.letterToGuess == ' ') {
+            this.hasThisLetterBeenGuessed = true;
+            return ' ';
+        } if(this.hasThisLetterBeenGuessed === true) {
+            return ' _ ';
+        } else {
+            return this.letterToGuess;
+        }
     };
 
-    this.updateGuessStatus = function () {
-        //update if the letter has been guessed or not T/F
-    };
+    // this.updateGuessStatus = function () {
+    //     //update if the letter has been guessed or not T/F
+    // };
 };
+
 
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,13 +1,51 @@
-// Word.js: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+// Word.js: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing 
+// the current word the user is attempting to guess. That means the constructor should define:
 
 
 // An array of new Letter objects representing the letters of the underlying word
-// A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+// A function that returns a string representing the word. This should call the function 
+// on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+
 // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
 
 var Letter = require("./letter.js");
 
-function Word () {
+function Word (randomWord) {
+
+    this.word = randomWord; //store the string randomWord - random word chosen from word list/array
+
+    this.letters = []; //push letters into this array
+
+    this.wordFound = false;
+
+    this.getRandomWordLetters = function () {
+        for (var i = 0; i < this.word.length; i++) {
+            var newLetter = new Letter(this.word[i]);
+            this.letters.push(newLetter);
+        };
+    };
+
+    // this.didWeFindTheWord
+
+    this.wasLetterFound = function (guessedLetter) {
+        // var whatToReturn = 0;
+        this.letters.forEach(function(letterToGuess) {
+            if(letterToGuess.letter === guessedLetter) {
+                letter.hasThisLetterBeenGuessed = true;
+
+            };
+        });
+    };
+
+    this.wordRender = function () {
+        var display = '';
+        this.letters.forEach(function(letterToGuess){
+            var currentLetter = letterToGuess.letterGuessResult();
+            display+= currentLetter;
+        });
+    };
+
+    return display;
 
 }
 
